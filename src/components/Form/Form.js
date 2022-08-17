e12999a8207f85cb33154cfb267d07ed7f4b9662
Sara Rossow <rossowka@icloud.com>
2022-08-17 15:05:30 +0200

    added functionality to read values

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,53 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Form.module.scss';
 
 function Form() {
+    const [formValue, setFormValue] = useState({
+        name: '',
+        email: '',
+        bio: '',
+        gender: '',
+    });
+
+    const [acceptLegal, setAcceptLegal] = useState(false);
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setFormValue({
+            ...formValue,
+            [name]: value
+        });
+    }
 
     return (
         <form>
             <h2>Registration Form</h2>
             <div>
                 <label htmlFor='name'>Name</label>
-                <input id='name' type='text' />
+                <input id='name' type='text' name='name' onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor='email'>E-mail</label>
-                <input id='email' type='text' />
+                <input id='email' type='text' name='email' onChange={handleChange} />
             </div>
             <div className={styles.bio}>
                 <label htmlFor='bio'>About</label>
-                <textarea id='bio'></textarea>
+                <textarea id='bio' name='bio' onChange={handleChange} ></textarea>
                 <label htmlFor='bio'>0/150</label>
             </div>
-            <div className={styles.genderChoices}>
+            <div className={styles.genderChoices} onChange={handleChange}>
             <h4>Which most accurately describes you?</h4>
                 <div>
-                    <input id='female' type='radio' name="select" />
+                    <input id='female' type='radio' name="gender" value='female' />
                     <label htmlFor='female'>Female</label>
                 </div>
                 <div>
-                    <input id='male' type='radio' name="select" />
+                    <input id='male' type='radio' name="gender" value='male' />
                     <label htmlFor='male'>Male</label>
                 </div>
                 <div>
-                    <input id='non-binary' type='radio' name="select" />
+                    <input id='non-binary' type='radio' name="gender" value='non-binary' />
                     <label htmlFor='non-binary'>Non-binary</label>
                 </div>
                 <div>
-                    <input id='transgender' type='radio' name="select" />
+                    <input id='transgender' type='radio' name="gender" value='transgender' />
                     <label htmlFor='transgender'>Transgender</label>
                 </div>
                 <div>
-                    <input id='intersex' type='radio' name="select" />
+                    <input id='intersex' type='radio' name="gender" value='intersex' />
                     <label htmlFor='intersex'>Intersex</label>
                 </div>
                 <div>
-                    <input id='unknown' type='radio' name="select" />
+                    <input id='unknown' type='radio' name="gender" value='unknown' />
                     <label htmlFor='unknown'>I prefer not to say</label>
                 </div>
             </div>
             <div className={styles.legal}>
-                <input id='legal' type='checkbox' />
+                <input id='legal' type='checkbox' onChange={(e) => setAcceptLegal(e.target.checked)} />
                 <span className={styles.checkbox}></span>
                 <label htmlFor='legal'>I accept <a href='#'>Terms &amp; Conditions</a></label>
             </div>
