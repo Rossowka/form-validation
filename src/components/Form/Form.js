6741ba9c2b152317db5febe0782dc3cc0f297525
Sara Rossow <rossowka@icloud.com>
2022-09-04 17:59:36 +0200

    added field validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './Form.module.scss';
 
 function Form() {
@@ -10,6 +10,16 @@ function Form() {
     });
 
     const [acceptLegal, setAcceptLegal] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const nameErrorRef = useRef(null);
+    const emailErrorRef = useRef(null);
+    const bioErrorRef = useRef(null);
+    const genderErrorRef = useRef(null);
+    const legalErrorRef = useRef(null);
+    const submitRef = useRef(null);
+
+    const { name, email, bio, gender } = formValue;
 
     const handleChange = (e) => {
         const name = e.target.name;
@@ -21,8 +31,7 @@ function Form() {
         });
 
         //initial validation
-        const letterCount = value.length;
-        if (letterCount === 0 ) {
+        if (value === '' ) {
             e.target.placeholder = 'Field required';
             e.target.style.border = `2px solid ${styles.errorColor}`;
         } else {
@@ -35,9 +44,60 @@ function Form() {
         e.preventDefault();
 
         //validation
-        
-        //send data
-        console.log(formValue);
+        if (name) {
+            nameErrorRef.current.placeholder = '';
+            nameErrorRef.current.style.border = `2px solid ${styles.primaryColor}`;
+        } else {
+            nameErrorRef.current.placeholder = 'Field required';
+            nameErrorRef.current.style.border = `2px solid ${styles.errorColor}`;
+        };
+
+        if (email) {
+            emailErrorRef.current.placeholder = '';
+            emailErrorRef.current.style.border = `2px solid ${styles.primaryColor}`;
+        } else {
+            emailErrorRef.current.placeholder = 'Field required';
+            emailErrorRef.current.style.border = `2px solid ${styles.errorColor}`;
+        };
+
+        if (bio) {
+            bioErrorRef.current.placeholder = '';
+            bioErrorRef.current.style.border = `2px solid ${styles.primaryColor}`;
+        } else {
+            bioErrorRef.current.placeholder = 'Field required';
+            bioErrorRef.current.style.border = `2px solid ${styles.errorColor}`;
+        };
+
+        if (gender === '') {
+            genderErrorRef.current.textContent = 'Choice required';
+        } else {
+            genderErrorRef.current.textContent = '';
+        };
+
+        if (acceptLegal) {
+            legalErrorRef.current.textContent = ''
+        } else {
+            legalErrorRef.current.textContent = 'Agreeing to T&C is required'
+        }
+
+        const formValuesFilled = Object.values(formValue).every(
+            value => value
+        );
+
+        if (acceptLegal && formValuesFilled) {
+            //send data
+            console.log(formValue, acceptLegal);
+            submitRef.current.textContent = 'Thank you for submitting your data!';
+            //clear form
+            setFormValue({
+                name: '',
+                email: '',
+                bio: '',
+                gender: ''
+            });
+            setAcceptLegal(false);
+            setIsDisabled(true);
+        };
     }
 
     return (
@@ -45,52 +105,112 @@ function Form() {
             <h2>Registration Form</h2>
             <div>
                 <label htmlFor='name'>Name</label>
-                <input id='name' type='text' name='name' onChange={handleChange} />
+                <input 
+                    id='name' 
+                    type='text' 
+                    name='name' 
+                    value={name} 
+                    onChange={handleChange} 
+                    ref={nameErrorRef}
+                />
             </div>
             <div>
                 <label htmlFor='email'>E-mail</label>
-                <input id='email' type='text' name='email' onChange={handleChange} />
+                <input 
+                    id='email' 
+                    type='text' 
+                    name='email' 
+                    value={email} 
+                    onChange={handleChange} 
+                    ref={emailErrorRef}
+                />
             </div>
             <div className={styles.bio}>
                 <label htmlFor='bio'>About</label>
-                <textarea id='bio' name='bio' onChange={handleChange}></textarea>
-                <label htmlFor='bio'>0/150</label>
+                <textarea 
+                    id='bio' 
+                    name='bio' 
+                    maxLength={150} 
+                    value={bio} 
+                    onChange={handleChange}
+                    ref={bioErrorRef}
+                ></textarea>
+                <label htmlFor='bio'>{formValue.bio.length}/150</label>
             </div>
             <div className={styles.genderChoices} onChange={handleChange}>
-            <h4>Which most accurately describes you?</h4>
+                <h4>Which most accurately describes you?</h4>
                 <div>
-                    <input id='female' type='radio' name="gender" value='female' />
+                    <input 
+                        id='female' 
+                        type='radio' 
+                        name='gender' 
+                        value='female' 
+                        checked={gender === 'female'} 
+                    />
                     <label htmlFor='female'>Female</label>
                 </div>
                 <div>
-                    <input id='male' type='radio' name="gender" value='male' />
+                    <input 
+                        id='male' 
+                        type='radio' 
+                        name='gender' 
+                        value='male' 
+                        checked={gender === 'male'}
+                    />
                     <label htmlFor='male'>Male</label>
                 </div>
                 <div>
-                    <input id='non-binary' type='radio' name="gender" value='non-binary' />
+                    <input 
+                        id='non-binary' 
+                        type='radio' 
+                        name='gender' 
+                        value='non-binary' 
+                        checked={gender === 'non-binary'}
+                    />
                     <label htmlFor='non-binary'>Non-binary</label>
                 </div>
                 <div>
-                    <input id='transgender' type='radio' name="gender" value='transgender' />
+                    <input 
+                        id='transgender' 
+                        type='radio' 
+                        name='gender' 
+                        value='transgender' 
+                        checked={gender === 'transgender'}
+                    />
                     <label htmlFor='transgender'>Transgender</label>
                 </div>
                 <div>
-                    <input id='intersex' type='radio' name="gender" value='intersex' />
+                    <input 
+                        id='intersex' 
+                        type='radio' 
+                        name='gender' 
+                        value='intersex' 
+                        checked={gender === 'intersex'}
+                    />
                     <label htmlFor='intersex'>Intersex</label>
                 </div>
                 <div>
-                    <input id='unknown' type='radio' name="gender" value='unknown' />
+                    <input 
+                        id='unknown' 
+                        type='radio' 
+                        name='gender' 
+                        value='unknown' 
+                        checked={gender === 'unknown'}
+                    />
                     <label htmlFor='unknown'>I prefer not to say</label>
                 </div>
+                <p className={styles.error} ref={genderErrorRef}></p>
             </div>
             <div className={styles.legal}>
-                <input id='legal' type='checkbox' onChange={(e) => setAcceptLegal(e.target.checked)} />
+                <input id='legal' type='checkbox' checked={acceptLegal} onChange={(e) => setAcceptLegal(e.target.checked)} />
                 <span className={styles.checkbox}></span>
                 <label htmlFor='legal'>I accept <a href='#'>Terms &amp; Conditions</a></label>
+                <p className={styles.error} ref={legalErrorRef}></p>
             </div>
             <div>
-                <input type='submit' value='Submit' className={styles.btn} />
+                <input type='submit' value='Submit' disabled={isDisabled} className={styles.btn} />
             </div>
+            <p className={styles.submit} ref={submitRef}></p>
         </form>
     );
 };
